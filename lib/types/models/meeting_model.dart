d988c14d93ce1d8f1c4412cb084eca6ad081bb7e
Kai <lambiengcode@gmail.com>
2024-10-27 12:26:00 +0700

    dart format .

--- lib/types/models/meeting_model.dart
+++ lib/types/models/meeting_model.dart
@@ -128,8 +128,7 @@ class Meeting {
           .getMeetingStatusEnum,
       createdAt: DateTime.parse(map['createdAt']).toLocal(),
       latestJoinedAt:
-          DateTime.parse(map['latestJoinedAt'] ?? map['createdAt'])
-              .toLocal(),
+          DateTime.parse(map['latestJoinedAt'] ?? map['createdAt']).toLocal(),
       latestMessage: map['latestMessage'] != null &&
               map['latestMessage'] is Map<String, dynamic>
           ? MessageModel.fromMap(map['latestMessage'])
